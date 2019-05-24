06d144ae3f61723633229539d2766f5b28234b01
latalavera <49410289+latalavera@users.noreply.github.com>
2019-05-24 17:22:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+#primero importamos las dos funciones creadas del archivo funciones.py
 from funciones import jugar
 from funciones import resultados
 
+#creamos un bucle que pregunte al usuario tres opciones y respondemos a ese input llamando a las funciones definidas anteriormente
 while True:
     selection = input("Would you like to a) play a new game, b) see the best scores, or c) quit? ")
-
     if selection == "a":
         jugar()
     elif selection == "b":
